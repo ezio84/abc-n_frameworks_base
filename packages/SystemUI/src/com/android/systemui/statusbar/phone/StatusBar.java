ddb9b1f2af52e938bb4299b3cb8775618f9c456c
ezio84 <brabus84@gmail.com>
2020-02-29 19:58:39 +0100

    Allow a wider back swipe to trigger actions (instead of timeout) [1/2]
    swipe for more than 3/4 of X axys display size to trigger action
    instead of long swipe timeout

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1832,6 +1832,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.BACK_SWIPE_EXTENDED),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -1865,7 +1868,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 setGestureNavOptions();
             } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_CLOCK_SELECTION))) {
                 updateKeyguardStatusSettings();
-            } else if (uri.equals(Settings.System.getUriFor(Settings.System.LONG_BACK_SWIPE_TIMEOUT)) ||
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.BACK_SWIPE_EXTENDED)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.LONG_BACK_SWIPE_TIMEOUT)) ||
                     uri.equals(Settings.System.getUriFor(Settings.System.LEFT_LONG_BACK_SWIPE_ACTION)) ||
                     uri.equals(Settings.System.getUriFor(Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION))) {
                 setGestureNavOptions();
